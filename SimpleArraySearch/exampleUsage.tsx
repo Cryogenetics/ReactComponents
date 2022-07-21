00ebf32a372152c47b5787cd1e1f3c1a8e99f766
Cryogenetics <Cryogenetics@ghostin.me>
2022-07-21 00:49:16 -0500

    formatting

--- SimpleArraySearch/exampleUsage.tsx
+++ SimpleArraySearch/exampleUsage.tsx
@@ -1,13 +1,12 @@
 import SearchBar from "../../components/shared/SearchBar";
-import { useState } from 'react';
+import { useState } from "react";
 import React from "react"; // required for some reason, don't know why
 
 //material UI icon imports
 import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 // requires @nextui-org/react
-// requires @mui/icons-material
-
+//requires
 
 export default function example() {
     const initialArray = [
@@ -15,7 +14,7 @@ export default function example() {
             name: "test",
             value: "test",
             numberValue: 1,
-        },      
+        },
         {
             name: "testa",
             value: "testa",
@@ -27,50 +26,52 @@ export default function example() {
 
     return (
         <>
-        <SearchBar
-            originalArray={originalArray || initialArray} // original array that doesn't change ( also showing you can pass in a static array)
-            setResult={setResultArray} // accepts setState or custom function
-            searchOptions={["name", "value"]} // keys you can search by
-            sortable // marks it as sortable (boolean, optional)
-            sortOptions={[
-                {
-                    name: "default", // displayValue
-                    func: () => {}, // return original array
-                },
-                {
-                    name: "alphabetical",
-                    func: function (a, b) {
-                        return a.name.localeCompare(b.key); // sort alphabetically
+            <SearchBar
+                originalArray={originalArray || initialArray} // original array that doesn't change ( also showing you can pass in a static array)
+                setResult={setResultArray} // accepts setState or custom function
+                searchOptions={["name", "value"]} // keys you can search by
+                sortable // marks it as sortable (boolean, optional)
+                sortOptions={[
+                    {
+                        name: "default", // displayValue
+                        func: () => {}, // return original array
                     },
-                },
-                {
-                    name: "value",
-                    func: function (a, b) {
-                        return b.numberValue - a.numberValue; // sort numbers
+                    {
+                        name: "alphabetical",
+                        func: function (a, b) {
+                            return a.name.localeCompare(b.key); // sort alphabetically
+                        },
                     },
-                },
-            ]}
-            reversible // mark that it can change desc/asc
-            AscIcon={
-                // icon to use for ascending
-                <ArrowUpwardIcon
-                color="primary"
-                aria-label="Switch to Descending"
-                />
-            }
-            DescIcon={
-                // icon to use for descending
-                <ArrowDownwardIcon
-                color="primary"
-                aria-label="Switch to Ascending"
-                />
-            }
+                    {
+                        name: "value",
+                        func: function (a, b) {
+                            return b.numberValue - a.numberValue; // sort numbers
+                        },
+                    },
+                ]}
+                reversible // mark that it can change desc/asc
+                AscIcon={
+                    // icon to use for ascending
+                    <ArrowUpwardIcon
+                        color="primary"
+                        aria-label="Switch to Descending"
+                    />
+                }
+                DescIcon={
+                    // icon to use for descending
+                    <ArrowDownwardIcon
+                        color="primary"
+                        aria-label="Switch to Ascending"
+                    />
+                }
             />
-            {
-                resultArray.map((k) => {
-                    return <div>{k.name} : {k.value}</div>;
-                })
-            }
-            </>
+            {resultArray.map((k) => {
+                return (
+                    <div>
+                        {k.name} : {k.value}
+                    </div>
+                );
+            })}
+        </>
     );
 }
